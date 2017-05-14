4c816da7c43e23312722a66d8257dd1eff7ca187
lavi061997 <lavi061997@gmail.com>
2017-05-14 12:54:23 +0530

    Done
    Angry Bird Replica

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Enemy : MonoBehaviour {
+	public GameObject deathEffect;
+	public float health = 1f;
+	public static int EnemiesAlive=0;
+
+	void Start()
+	{
+		EnemiesAlive++;
+	}
+	void OnCollisionEnter2D(Collision2D colInfo)
+	{
+		if(colInfo.relativeVelocity.magnitude > health)
+		{
+			Die();
+		}
+	}
+	void Die()
+	{
+		Instantiate (deathEffect, transform.position, Quaternion.identity);
+		EnemiesAlive--;
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/ball.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ball : MonoBehaviour {
+
+	private bool isPressed = false;
+	public float releaseTime = 0.15f;
+	public Rigidbody2D rb;
+	public Rigidbody2D hook;
+	public GameObject nextBall;
+	public float maxDragDistance = 15f;
+	void Update()
+	{
+		if (isPressed) 
+		{
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			if(Vector3.Distance(mousePos,hook.position) > maxDragDistance)
+			{
+				rb.position = hook.position + (mousePos  - hook.position).normalized *maxDragDistance;
+			}
+			else
+			rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		}
+
+	}
+
+
+	void OnMouseDown()
+	{
+		isPressed = true;
+		rb.isKinematic = true; 
+	}
+
+	void OnMouseUp()
+	{ 
+		isPressed = false;
+		rb.isKinematic = false;
+		StartCoroutine (Release ());
+	}
+
+	IEnumerator Release()
+	{
+		yield return new WaitForSeconds (releaseTime);
+		GetComponent<SpringJoint2D>().enabled = false;
+		this.enabled = false;
+		yield return new WaitForSeconds(2f);
+		if (nextBall != null) {
+			nextBall.SetActive (true);
+		}
+
+
+	}
+
+}
